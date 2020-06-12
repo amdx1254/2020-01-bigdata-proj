54e535096a0e7fc628f665f8c100f24b9baca2d1
Hong <tjdgus0454@gmail.com>
2020-06-12 19:19:50 +0900

    make indexpage

--- web/app.js
+++ web/app.js
@@ -6,8 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var filteringRouter = require('./routes/api/filtering');
+
+var livereload = require("livereload").createServer({
+    exts: ['html', 'css', 'ejs'],
+    debug: true
+});
 
 var app = express();
+
+livereload.watch(path.join(__dirname, 'views'));
+livereload.watch(path.join(__dirname, 'public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,21 +30,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/filtering', filteringRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
