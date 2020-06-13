e94f267ee830cc418b22bd5c0147b6c86779a91f
Hong <tjdgus0454@gmail.com>
2020-06-13 22:19:19 +0900

    addFilteringChart

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -1,8 +1,4 @@
 (function($) {
-    let cfnlist = [];
-    let cfslist = [];
-    let cbfnlist = [];
-    let cbfslist = [];
     // Set new default font family and font color to mimic Bootstrap's default styling
     Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
     Chart.defaults.global.defaultFontColor = '#858796';
@@ -31,141 +27,144 @@
         }
         return s.join(dec);
     }
-    let createLineChart = function() {
+    let createLineChart = function(arrx, cflist, cbflist) {
 
-        console.log(cfnlist);
-        console.log(cfnlist.length);
-        // let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
-        // let cbfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+        let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+        let cbfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
 
-        // for (let idx in arrx) {
-        //     for (let cfy of arry1) {
-        //         if (cfy.id == arrx[idx]) {
-        //             console.log("test");
-        //             cfAreaY[idx] = cfy.satisfaction;
-        //         }
-        //     }
-        //     for (let cbfy of arry2) {
-        //         if (cbfy.id == arrx[idx]) {
-        //             cbfAreaY[idx] = cbfy.satisfaction;
-        //         }
-        //     }
-        // }
-        // // areaChart Start
-        // var ctx = document.getElementById("myAreaChart");
-        // var myLineChart = new Chart(ctx, {
-        //     type: 'line',
-        //     data: {
-        //         labels: arrx,
-        //         datasets: [{
-        //             label: "Collaborative filtering",
-        //             lineTension: 0.3,
-        //             backgroundColor: "rgba(78, 115, 223, 0.05)",
-        //             borderColor: "rgba(78, 115, 223, 1)",
-        //             pointRadius: 3,
-        //             pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        //             pointBorderColor: "rgba(78, 115, 223, 1)",
-        //             pointHoverRadius: 3,
-        //             pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        //             pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        //             pointHitRadius: 10,
-        //             pointBorderWidth: 2,
-        //             data: cfAreaY,
-        //         }, {
-        //             label: "Content Based filtering",
-        //             lineTension: 0.3,
-        //             backgroundColor: "rgba(78, 115, 223, 0.05)",
-        //             borderColor: "rgba(78, 115, 223, 1)",
-        //             pointRadius: 3,
-        //             pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        //             pointBorderColor: "rgba(78, 115, 223, 1)",
-        //             pointHoverRadius: 3,
-        //             pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        //             pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        //             pointHitRadius: 10,
-        //             pointBorderWidth: 2,
-        //             data: cbfAreaY,
-        //         }],
-        //     },
-        //     options: {
-        //         maintainAspectRatio: false,
-        //         layout: {
-        //             padding: {
-        //                 left: 10,
-        //                 right: 25,
-        //                 top: 25,
-        //                 bottom: 0
-        //             }
-        //         },
-        //         scales: {
-        //             xAxes: [{
-        //                 display: true,
-        //                 scaleLabel: {
-        //                     display: true,
-        //                     labelString: 'id'
-        //                 }
-        //             }],
-        //             yAxes: [{
-        //                 ticks: {
-        //                     maxTicksLimit: 5,
-        //                     padding: 10,
-        //                     // Include a dollar sign in the ticks
-        //                     callback: function(value, index, values) {
-        //                         return '$' + number_format(value);
-        //                     }
-        //                 },
-        //                 gridLines: {
-        //                     color: "rgb(234, 236, 244)",
-        //                     zeroLineColor: "rgb(234, 236, 244)",
-        //                     drawBorder: false,
-        //                     borderDash: [2],
-        //                     zeroLineBorderDash: [2]
-        //                 }
-        //             }],
-        //         },
-        //         legend: {
-        //             display: false
-        //         },
-        //         tooltips: {
-        //             backgroundColor: "rgb(255,255,255)",
-        //             bodyFontColor: "#858796",
-        //             titleMarginBottom: 10,
-        //             titleFontColor: '#6e707e',
-        //             titleFontSize: 14,
-        //             borderColor: '#dddfeb',
-        //             borderWidth: 1,
-        //             xPadding: 15,
-        //             yPadding: 15,
-        //             displayColors: false,
-        //             intersect: false,
-        //             mode: 'index',
-        //             caretPadding: 10,
-        //             callbacks: {
-        //                 label: function(tooltipItem, chart) {
-        //                     var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-        //                     return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // });
+        for (let idx in arrx) {
+            for (let cfy of cflist) {
+                if (cfy.name == arrx[idx]) {
+                    console.log("test");
+                    cfAreaY[idx] = cfy.satisfaction;
+                }
+            }
+            for (let cbfy of cbflist) {
+                if (cbfy.name == arrx[idx]) {
+                    cbfAreaY[idx] = cbfy.satisfaction;
+                }
+            }
+        }
+        // areaChart Start
+        var ctx = document.getElementById("myAreaChart");
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: arrx,
+                datasets: [{
+                    label: "Collaborative filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cfAreaY,
+                }, {
+                    label: "Content Based filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(115, 223, 78, 0.05)",
+                    borderColor: "rgba(115, 223, 78, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointHoverBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cbfAreaY,
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                        fontColor: 'black'
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'name'
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            maxTicksLimit: 5,
+                            padding: 10,
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, values) {
+                                return number_format(value) + '%';
+                            }
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    intersect: false,
+                    mode: 'index',
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function(tooltipItem, chart) {
+                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                            return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + '%';
+                        }
+                    }
+                }
+            }
+        });
         //  areaChart End
     }
 
     $(document).on('click', '.search-btn', function(e) {
         let user_id = $('#user-input').text();
         $('.cf-item').empty();
+        let cflist = [];
+        let cbflist = [];
+        let arrx = new Set();
         $.ajax({
             method: "GET",
             url: "http://localhost:3000/api/filtering/cf",
+            async: false,
             data: { user_id: user_id },
             success: function(data) {
-                console.log(data.item);
                 for (let i of data.item) {
-                    let name = i.name;
-                    cfnlist.push(name);
-                    let satisfaction = i.satisfaction;
-                    cfslist.push(satisfaction);
+                    cflist.push(i);
+                    arrx.add(i.name);
                     let at = '                          <div class="col-xl-3 col-md-6 mb-4">\
                     <div class="card border-left-info shadow h-100 py-2">\
                         <div class="card-body">\
@@ -199,13 +198,12 @@
         $.ajax({
             method: "GET",
             url: "http://localhost:3000/api/filtering/cbf",
+            async: false,
             data: { user_id: user_id },
             success: function(data) {
                 for (let i of data.item) {
-                    let name = i.name;
-                    cbfnlist.push(name);
-                    let satisfaction = i.satisfaction;
-                    cbfslist.push(satisfaction);
+                    cbflist.push(i);
+                    arrx.add(i.name);
                     let at = '                          <div class="col-xl-3 col-md-6 mb-4">\
                     <div class="card border-left-info shadow h-100 py-2">\
                         <div class="card-body">\
@@ -235,7 +233,7 @@
                 }
             }
         });
-        createLineChart();
+        createLineChart(Array.from(arrx), cflist, cbflist);
     });
 
 })(jQuery);
