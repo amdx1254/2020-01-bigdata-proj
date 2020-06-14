bd18d627871761da8fe50348aeee272ce19c4491
Hong <tjdgus0454@gmail.com>
2020-06-15 00:37:25 +0900

    addCategoryChart

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -27,7 +27,131 @@
         }
         return s.join(dec);
     }
-    let createLineChart = function(arrx, cflist, cbflist) {
+
+    let createCategoryLineChart = function(arrx, cflist, cbflist) {
+
+        let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+        let cbfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+
+        for (let idx in arrx) {
+            for (let cfy of cflist) {
+                if (cfy.category == arrx[idx]) {
+                    cfAreaY[idx] += 1;
+                }
+            }
+            for (let cbfy of cbflist) {
+                if (cbfy.category == arrx[idx]) {
+                    cbfAreaY[idx] += 1;
+                }
+            }
+        }
+        console.log(cbfAreaY);
+        // areaChart Start
+        var ctx = document.getElementById("myCategoryChart");
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: arrx,
+                datasets: [{
+                    label: "Collaborative filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cfAreaY,
+                }, {
+                    label: "Content Based filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(115, 223, 78, 0.05)",
+                    borderColor: "rgba(115, 223, 78, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointHoverBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cbfAreaY,
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                        fontColor: 'black'
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Category'
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            maxTicksLimit: 5,
+                            padding: 10,
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, values) {
+                                return number_format(value);
+                            }
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    intersect: false,
+                    mode: 'index',
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function(tooltipItem, chart) {
+                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                            return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                        }
+                    }
+                }
+            }
+        });
+        //  areaChart End
+    }
+
+    let createScoreLineChart = function(arrx, cflist, cbflist) {
 
         let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
         let cbfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
@@ -35,18 +159,17 @@
         for (let idx in arrx) {
             for (let cfy of cflist) {
                 if (cfy.name == arrx[idx]) {
-                    console.log("test");
-                    cfAreaY[idx] = cfy.satisfaction;
+                    cfAreaY[idx] = cfy.score;
                 }
             }
             for (let cbfy of cbflist) {
                 if (cbfy.name == arrx[idx]) {
-                    cbfAreaY[idx] = cbfy.satisfaction;
+                    cbfAreaY[idx] = cbfy.score;
                 }
             }
         }
         // areaChart Start
-        var ctx = document.getElementById("myAreaChart");
+        var ctx = document.getElementById("myScoreChart");
         var myLineChart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -155,7 +278,8 @@
         $('.cf-item').empty();
         let cflist = [];
         let cbflist = [];
-        let arrx = new Set();
+        let arrName = new Set();
+        let arrCategory = new Set();
         $.ajax({
             method: "GET",
             url: "http://localhost:3000/api/filtering/cf",
@@ -164,7 +288,8 @@
             success: function(data) {
                 for (let i of data.item) {
                     cflist.push(i);
-                    arrx.add(i.name);
+                    arrName.add(i.name);
+                    arrCategory.add(i.category);
                     let at = '                          <div class="col-xl-3 col-md-6 mb-4">\
                     <div class="card border-left-info shadow h-100 py-2">\
                         <div class="card-body">\
@@ -173,11 +298,11 @@
                                     <div class="text-xs font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
                                     <div class="row no-gutters align-items-center">\
                                         <div class="col-auto">\
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.satisfaction + '%</div>\
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.score + '%</div>\
                                         </div>\
                                         <div class="col">\
                                             <div class="progress progress-sm mr-2">\
-                                                <div class="progress-bar bg-info" role="progressbar" style="width: ' + i.satisfaction + '%" aria-valuenow="' + i.satisfaction + '" aria-valuemin="0" aria-valuemax="100"></div>\
+                                                <div class="progress-bar bg-info" role="progressbar" style="width: ' + i.score + '%" aria-valuenow="' + i.score + '" aria-valuemin="0" aria-valuemax="100"></div>\
                                             </div>\
                                         </div>\
                                     </div>\
@@ -203,7 +328,8 @@
             success: function(data) {
                 for (let i of data.item) {
                     cbflist.push(i);
-                    arrx.add(i.name);
+                    arrName.add(i.name);
+                    arrCategory.add(i.category);
                     let at = '                          <div class="col-xl-3 col-md-6 mb-4">\
                     <div class="card border-left-info shadow h-100 py-2">\
                         <div class="card-body">\
@@ -212,11 +338,11 @@
                                     <div class="text-xs font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
                                     <div class="row no-gutters align-items-center">\
                                         <div class="col-auto">\
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.satisfaction + '%</div>\
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.score + '%</div>\
                                         </div>\
                                         <div class="col">\
                                             <div class="progress progress-sm mr-2">\
-                                                <div class="progress-bar bg-info" role="progressbar" style="width: ' + i.satisfaction + '%" aria-valuenow="' + i.satisfaction + '" aria-valuemin="0" aria-valuemax="100"></div>\
+                                                <div class="progress-bar bg-info" role="progressbar" style="width: ' + i.score + '%" aria-valuenow="' + i.score + '" aria-valuemin="0" aria-valuemax="100"></div>\
                                             </div>\
                                         </div>\
                                     </div>\
@@ -233,7 +359,8 @@
                 }
             }
         });
-        createLineChart(Array.from(arrx), cflist, cbflist);
+        createScoreLineChart(Array.from(arrName), cflist, cbflist);
+        createCategoryLineChart(Array.from(arrCategory), cflist, cbflist);
     });
 
 })(jQuery);
--- web/routes/api/filtering.js
+++ web/routes/api/filtering.js
@@ -10,7 +10,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/cf', function(req, res, next) {
     var user_id = req.query.user_id;
-    var item = [{ id: 1, name: "테스트1", satisfaction: 30 }, { id: 2, name: "테스트2", satisfaction: 10 }]
+    var item = [{ id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", score: 95 },
+        { id: 2, name: "테스트2", summary: "testsummary2", category: "testcategory2", makerName: "testmakerName2", score: 90 },
+        { id: 5, name: "테스트5", summary: "testsummary5", category: "testcategory1", makerName: "testmakerName1", score: 80 },
+        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", score: 80 },
+        { id: 9, name: "테스트9", summary: "testsummary9", category: "testcategory3", makerName: "testmakerName3", score: 80 },
+        { id: 10, name: "테스트10", summary: "testsummary10", category: "testcategory4", makerName: "testmakerName4", score: 75 },
+        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", score: 70 },
+        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", score: 60 }
+    ];
     res.send({
         item: item
     });
@@ -18,7 +26,15 @@ router.get('/cf', function(req, res, next) {
 
 router.get('/cbf', function(req, res, next) {
     var user_id = req.query.user_id;
-    var item = [{ id: 1, name: "테스트1", satisfaction: 70 }, { id: 3, name: "테스트3", satisfaction: 60 }]
+    var item = [{ id: 3, name: "테스트3", summary: "testsummary3", category: "testcategory3", makerName: "testmakerName3", score: 90 },
+        { id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", score: 80 },
+        { id: 4, name: "테스트4", summary: "testsummary4", category: "testcategory2", makerName: "testmakerName2", score: 70 },
+        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", score: 70 },
+        { id: 7, name: "테스트7", summary: "testsummary7", category: "testcategory5", makerName: "testmakerName5", score: 70 },
+        { id: 8, name: "테스트8", summary: "testsummary8", category: "testcategory5", makerName: "testmakerName5", score: 70 },
+        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", score: 70 },
+        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", score: 60 }
+    ]
     res.send({
         item: item
     });
