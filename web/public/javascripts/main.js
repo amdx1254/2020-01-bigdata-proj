362da1f2eae6efdf2b3f35442bdb9095a5b0b906
Hong <tjdgus0454@gmail.com>
2020-06-15 01:34:57 +0900

    addAmountChart

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -28,6 +28,151 @@
         return s.join(dec);
     }
 
+    let createAmountLineChart = function(cflist, cbflist) {
+        let arrx = ['0-10000', '10000-20000', '20000-30000', '30000-40000', '40000-50000', '50000-100000', '100000-'];
+        let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+        let cbfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
+
+        for (let cfy of cflist) {
+            if (cfy.amount < 10000 && cfy.amount >= 0) {
+                cfAreaY[0] += 1;
+            } else if (cfy.amount < 20000 && cfy.amount >= 10000) {
+                cfAreaY[1] += 1;
+            } else if (cfy.amount < 30000 && cfy.amount >= 20000) {
+                cfAreaY[2] += 1;
+            } else if (cfy.amount < 40000 && cfy.amount >= 30000) {
+                cfAreaY[3] += 1;
+            } else if (cfy.amount < 50000 && cfy.amount >= 40000) {
+                cfAreaY[4] += 1;
+            } else if (cfy.amount < 100000 && cfy.amount >= 50000) {
+                cfAreaY[5] += 1;
+            } else {
+                cfAreaY[6] += 1;
+            }
+        }
+        for (let cbfy of cbflist) {
+            if (cbfy.amount < 10000 && cbfy.amount >= 0) {
+                cbfAreaY[0] += 1;
+            } else if (cbfy.amount < 20000 && cbfy.amount >= 10000) {
+                cbfAreaY[1] += 1;
+            } else if (cbfy.amount < 30000 && cbfy.amount >= 20000) {
+                cbfAreaY[2] += 1;
+            } else if (cbfy.amount < 40000 && cbfy.amount >= 30000) {
+                cbfAreaY[3] += 1;
+            } else if (cbfy.amount < 50000 && cbfy.amount >= 40000) {
+                cbfAreaY[4] += 1;
+            } else if (cbfy.amount < 100000 && cbfy.amount >= 50000) {
+                cbfAreaY[5] += 1;
+            } else {
+                cbfAreaY[6] += 1;
+            }
+        }
+        console.log(cbfAreaY);
+        // areaChart Start
+        var ctx = document.getElementById("myAmountChart");
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: arrx,
+                datasets: [{
+                    label: "Collaborative filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(78, 115, 223, 0.05)",
+                    borderColor: "rgba(78, 115, 223, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+                    pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cfAreaY,
+                }, {
+                    label: "Content Based filtering",
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(115, 223, 78, 0.05)",
+                    borderColor: "rgba(115, 223, 78, 1)",
+                    pointRadius: 3,
+                    pointBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHoverRadius: 3,
+                    pointHoverBackgroundColor: "rgba(115, 223, 78, 1)",
+                    pointHoverBorderColor: "rgba(115, 223, 78, 1)",
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    data: cbfAreaY,
+                }],
+            },
+            options: {
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                        fontColor: 'black'
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Amount'
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            maxTicksLimit: 5,
+                            padding: 10,
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, values) {
+                                return number_format(value);
+                            }
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    intersect: false,
+                    mode: 'index',
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function(tooltipItem, chart) {
+                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                            return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                        }
+                    }
+                }
+            }
+        });
+        //  areaChart End
+    }
+
     let createCategoryLineChart = function(arrx, cflist, cbflist) {
 
         let cfAreaY = Array.apply(null, new Array(arrx.length)).map(Number.prototype.valueOf, 0);
@@ -295,7 +440,8 @@
                         <div class="card-body">\
                             <div class="row no-gutters align-items-center">\
                                 <div class="col mr-2">\
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
+                                    <div class="text-s font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">' + i.amount + '원</div>\
                                     <div class="row no-gutters align-items-center">\
                                         <div class="col-auto">\
                                             <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.score + '%</div>\
@@ -335,7 +481,8 @@
                         <div class="card-body">\
                             <div class="row no-gutters align-items-center">\
                                 <div class="col mr-2">\
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
+                                    <div class="text-s font-weight-bold text-info text-uppercase mb-1">' + i.name + '</div>\
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">' + i.amount + '원</div>\
                                     <div class="row no-gutters align-items-center">\
                                         <div class="col-auto">\
                                             <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">' + i.score + '%</div>\
@@ -361,6 +508,7 @@
         });
         createScoreLineChart(Array.from(arrName), cflist, cbflist);
         createCategoryLineChart(Array.from(arrCategory), cflist, cbflist);
+        createAmountLineChart(cflist, cbflist);
     });
 
 })(jQuery);
--- web/routes/api/filtering.js
+++ web/routes/api/filtering.js
@@ -10,14 +10,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/cf', function(req, res, next) {
     var user_id = req.query.user_id;
-    var item = [{ id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", score: 95 },
-        { id: 2, name: "테스트2", summary: "testsummary2", category: "testcategory2", makerName: "testmakerName2", score: 90 },
-        { id: 5, name: "테스트5", summary: "testsummary5", category: "testcategory1", makerName: "testmakerName1", score: 80 },
-        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", score: 80 },
-        { id: 9, name: "테스트9", summary: "testsummary9", category: "testcategory3", makerName: "testmakerName3", score: 80 },
-        { id: 10, name: "테스트10", summary: "testsummary10", category: "testcategory4", makerName: "testmakerName4", score: 75 },
-        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", score: 70 },
-        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", score: 60 }
+    var item = [{ id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", amount: 50000, score: 95 },
+        { id: 2, name: "테스트2", summary: "testsummary2", category: "testcategory2", makerName: "testmakerName2", amount: 20000, score: 90 },
+        { id: 5, name: "테스트5", summary: "testsummary5", category: "testcategory1", makerName: "testmakerName1", amount: 100000, score: 80 },
+        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", amount: 50000, score: 80 },
+        { id: 9, name: "테스트9", summary: "testsummary9", category: "testcategory3", makerName: "testmakerName3", amount: 30000, score: 80 },
+        { id: 10, name: "테스트10", summary: "testsummary10", category: "testcategory4", makerName: "testmakerName4", amount: 50000, score: 75 },
+        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", amount: 50000, score: 70 },
+        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", amount: 5000, score: 60 }
     ];
     res.send({
         item: item
@@ -26,14 +26,14 @@ router.get('/cf', function(req, res, next) {
 
 router.get('/cbf', function(req, res, next) {
     var user_id = req.query.user_id;
-    var item = [{ id: 3, name: "테스트3", summary: "testsummary3", category: "testcategory3", makerName: "testmakerName3", score: 90 },
-        { id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", score: 80 },
-        { id: 4, name: "테스트4", summary: "testsummary4", category: "testcategory2", makerName: "testmakerName2", score: 70 },
-        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", score: 70 },
-        { id: 7, name: "테스트7", summary: "testsummary7", category: "testcategory5", makerName: "testmakerName5", score: 70 },
-        { id: 8, name: "테스트8", summary: "testsummary8", category: "testcategory5", makerName: "testmakerName5", score: 70 },
-        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", score: 70 },
-        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", score: 60 }
+    var item = [{ id: 3, name: "테스트3", summary: "testsummary3", category: "testcategory3", makerName: "testmakerName3", amount: 50000, score: 90 },
+        { id: 1, name: "테스트1", summary: "testsummary1", category: "testcategory1", makerName: "testmakerName1", amount: 50000, score: 80 },
+        { id: 4, name: "테스트4", summary: "testsummary4", category: "testcategory2", makerName: "testmakerName2", amount: 40000, score: 70 },
+        { id: 6, name: "테스트6", summary: "testsummary6", category: "testcategory1", makerName: "testmakerName2", amount: 10000, score: 70 },
+        { id: 7, name: "테스트7", summary: "testsummary7", category: "testcategory5", makerName: "testmakerName5", amount: 150000, score: 70 },
+        { id: 8, name: "테스트8", summary: "testsummary8", category: "testcategory5", makerName: "testmakerName5", amount: 50000, score: 70 },
+        { id: 11, name: "테스트11", summary: "testsummary11", category: "testcategory1", makerName: "testmakerName1", amount: 10000, score: 70 },
+        { id: 12, name: "테스트12", summary: "testsummary12", category: "testcategory5", makerName: "testmakerName5", amount: 5000, score: 60 }
     ]
     res.send({
         item: item
